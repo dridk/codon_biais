dcee0ce3161118ae232c35a42e77e4d9a5b8d914
dridk <sacha@labsquare.org>
2018-01-05 17:52:31 +0100

    add resuts

--- scripts/plot.r
+++ scripts/plot.r
@@ -1,4 +1,28 @@
-data = read.table("final_codon.data", sep="\t", header=T)
 
-data$X <- NULL
+library(ggplot2)
+library(ade4)
+data(aminoacyl)
+codons = read.table("final_codon.data", sep="\t", header=T)
+d = codons
+d$gene = NULL
+d$X = NULL
+
+
+ggdata = data.frame(count=apply(d,2,sum))
+aanames = sapply(rownames(ggdata), function(x) {aminoacyl$AA[which(aminoacyl$codon == x)]})
+
+ggdata$aa = as.factor(aanames)
+ggdata$codon = as.factor(rownames(ggdata))
+
+ggdata$codon2 = NULL
+
+g = ggplot(ggdata, aes(x = codon, y = count, fill=aa))
+g + geom_histogram(stat="identity")+theme(axis.text = element_text(angle=90))
+
+rownames(d) = codons$gene
+
+g.dist = dist(d)
+
+
+
 
